828193f50db29e945df86f2ad7e4af82e65af355
Greg Law <greg.law@ntlworld.com>
2016-01-27 18:06:22 +0000

    Tested basic app and added bounds on zoom and longitude
    The testing went completely fine it all worked as expected. There is no simple
    way to add bounds on these values. Instead the class now listens for camera
    change events from the map and repositions the view afterwards. There was
    initially a problem with this if the user exceeded two bounds in one movement
    to fix this instead of animating for each bound that was exceeded a new
    position is created based on all of the bounds and the current position. Then
    the map is animated to this new position.

--- app/src/main/java/com/solutions/law/universityrouteplanner/outdoorActivity.java
+++ app/src/main/java/com/solutions/law/universityrouteplanner/outdoorActivity.java
@@ -1,16 +1,26 @@
 package com.solutions.law.universityrouteplanner;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.graphics.Color;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.GoogleMap.OnCameraChangeListener;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 
-public class outdoorActivity extends FragmentActivity implements OnMapReadyCallback {
+
+public class outdoorActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnCameraChangeListener {
 
     private GoogleMap mMap;
 
@@ -37,9 +47,55 @@ public class outdoorActivity extends FragmentActivity implements OnMapReadyCallb
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        LatLng centre = new LatLng(55.861903, -4.244082);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(centre, 16));
+        Polygon polygon = mMap.addPolygon(new PolygonOptions()
+                .add(new LatLng(55.861830,-4.247068), new LatLng(55.861788,-4.246526), new LatLng(55.862038,-4.246440)
+                        , new LatLng(55.861889,-4.245188), new LatLng(55.861053,-4.245488),new LatLng(55.861272,-4.247248))
+                .strokeColor(Color.BLUE)
+                .fillColor(Color.CYAN));
+        mMap.getUiSettings().setIndoorLevelPickerEnabled(false);
 
-        // Add a marker in Sydney and move the camera
-        LatLng centre = new LatLng(55.862056, -4.239915);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(centre));
+        mMap.setIndoorEnabled(false);
+        Paint paint = new Paint();
+        paint.setTextSize(28);
+        paint.setColor(Color.BLACK);
+        paint.setTextAlign(Paint.Align.LEFT);
+        float baseline = -paint.ascent(); // ascent() is negative
+        int width = (int) (paint.measureText("Royal College") + 0.5f); // round
+        int height = (int) (baseline + paint.descent() + 0.5f);
+        Bitmap image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(image);
+        canvas.drawText("Royal College", 0, baseline, paint);
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(55.861477, -4.246285))
+                .icon(BitmapDescriptorFactory.fromBitmap(image)));
+        mMap.setOnCameraChangeListener(this);
+
+    }
+
+    @Override
+    public void onCameraChange(CameraPosition position){
+        //Instead of animating each time build new cameraPosition then animate to there
+        //this will stop you missing it if they zoom and move out of bounds.
+        CameraPosition newPosition;
+        LatLng newLatLng =position.target;
+        float newZoom=position.zoom;
+        if(position.zoom<16) {
+            newZoom=16;
+        }else if(position.zoom>17){
+            newZoom=17;
+        }
+        if(position.target.longitude<-4.250000){
+            newLatLng=new LatLng(position.target.latitude,-4.250000);
+        }
+        if(position.target.longitude>-4.235812){
+            newLatLng=new LatLng(position.target.latitude,-4.235812);
+        }
+        newPosition = new CameraPosition(newLatLng,newZoom,position.tilt,position.bearing);
+        if(!newPosition.equals(position)) {
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(newPosition));
+        }
     }
 }
